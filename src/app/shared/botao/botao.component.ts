2684548b712747357a8494b1e3862bd39708cd53
francelino <fmarcilio1989@gmail.com>
2022-07-14 15:13:53 -0300

    implementando o component de botao

--- src/app/shared/botao/botao.component.ts
+++ src/app/shared/botao/botao.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-botao',
   templateUrl: './botao.component.html',
-  styleUrls: ['./botao.component.scss']
+  styleUrls: ['./botao.component.scss'],
 })
 export class BotaoComponent implements OnInit {
+  @Input() public texto?: string;
+  @Input() public cor?: string = 'primaria';
+  @Input() public classCss: string = '';
+  @Input() public tipo: string = 'button';
+  @Input() public desabilitar: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
