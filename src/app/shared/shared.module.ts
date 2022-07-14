1340e6e359fbb19fdccc572008d984d2c6076701
francelino <fmarcilio1989@gmail.com>
2022-07-14 13:40:00 -0300

    modulo de shared e component de botao

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BotaoComponent } from './botao/botao.component';
+
+@NgModule({
+  declarations: [BotaoComponent],
+  imports: [CommonModule],
+  exports: [BotaoComponent],
+})
+export class SharedModule {}
