1a5595f86981c0ac367c92724e5092e6c155318f
francelino <fmarcilio1989@gmail.com>
2022-07-14 16:07:20 -0300

    implementando o component de avatar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BotaoComponent } from './botao/botao.component';
+import { AvatarComponent } from './avatar/avatar.component';
 
 @NgModule({
-  declarations: [BotaoComponent],
+  declarations: [BotaoComponent, AvatarComponent],
   imports: [CommonModule],
-  exports: [BotaoComponent],
+  exports: [BotaoComponent, AvatarComponent],
 })
 export class SharedModule {}
