96b02841ac0c638e243253ca4f62de6a0ab23b57
francelino <fmarcilio1989@gmail.com>
2022-07-14 18:02:24 -0300

    desenvolvendo o upload

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BotaoComponent } from './botao/botao.component';
 import { AvatarComponent } from './avatar/avatar.component';
+import { UploadImagemComponent } from './upload-imagem/upload-imagem.component';
 
 @NgModule({
-  declarations: [BotaoComponent, AvatarComponent],
+  declarations: [BotaoComponent, AvatarComponent, UploadImagemComponent],
   imports: [CommonModule],
-  exports: [BotaoComponent, AvatarComponent],
+  exports: [BotaoComponent, AvatarComponent, UploadImagemComponent],
 })
 export class SharedModule {}
