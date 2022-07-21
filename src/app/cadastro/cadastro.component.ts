168e4bdda468b5ff174737114ab54ba2bfc408da
Francelino <fmarcilio1989@gmail.com>
2022-07-21 15:19:43 -0300

    adicionando validação customizada para senha e criando a tela de cadastro de usuário

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { confirmacaoSenha } from '../shared/validadores/confirmacao-senha.validator';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.scss']
+  styleUrls: ['./cadastro.component.scss'],
 })
 export class CadastroComponent implements OnInit {
+  public form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(4)]],
+      confirmacaoSenha: ['', [Validators.required, confirmacaoSenha]],
+    });
   }
 
+  ngOnInit(): void {}
+
+  public obterReferencia(nomeCampo: string): AbstractControl {
+    return this.form.controls[nomeCampo];
+  }
+
+  public aoSubmeter() {
+    console.log('teste submit');
+  }
 }
--- src/app/cadastro/cadastro.module.ts
+++ src/app/cadastro/cadastro.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CadastroRoutingModule } from './cadastro-routing.module';
 import { CadastroComponent } from './cadastro.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [CadastroComponent],
-  imports: [CommonModule, CadastroRoutingModule],
+  imports: [CommonModule, CadastroRoutingModule, SharedModule],
   exports: [CadastroComponent],
 })
 export class CadastroModule {}
