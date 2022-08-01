da3fe935af43b2f7bc749780d9db55820aa7c3f9
Francelino <fmarcilio1989@gmail.com>
2022-08-01 14:46:21 -0300

    adicionando logica da opção mais para exibir descrição completa

--- src/app/shared/components/feed/feed.component.ts
+++ src/app/shared/components/feed/feed.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Postagem } from './postagem.type';
 
+const limiteCaracteresDescricaoPadrao = 90;
+
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss'],
 })
 export class FeedComponent implements OnInit {
+  public limiteCaracteresDescricao: number = limiteCaracteresDescricaoPadrao;
   public postagens: Array<Postagem> = [];
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  public exibirDescricaoCompleta() {
+    this.limiteCaracteresDescricao = 99999999;
+  }
 }
