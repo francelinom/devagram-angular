f226b34b4da1af1e0240b697ca397be0bb0f0c80
Francelino <fmarcilio1989@gmail.com>
2022-08-11 16:35:37 -0300

    adicionando metodo para permitir arrastar e soltar a imagem

--- src/app/shared/components/upload-imagem/upload-imagem.component.ts
+++ src/app/shared/components/upload-imagem/upload-imagem.component.ts
@@ -17,17 +17,34 @@ export class UploadImagemComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  private fazerUploadImagem(arquivo: File) {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(arquivo);
+    fileReader.onloadend = () => {
+      this.imagemPrevisualizacao = fileReader.result as string;
+      this.referenciaFormulario?.setValue(arquivo);
+      this.referenciaFormulario?.markAsDirty();
+      this.aoAtualizarImagem.emit(this.imagemPrevisualizacao);
+    };
+  }
+
   public trocarArquivo(event: any): void {
     if (event.target.files && event.target.files.length) {
       const [arquivo] = event.target.files;
-      const fileReader = new FileReader();
-      fileReader.readAsDataURL(arquivo);
-      fileReader.onloadend = () => {
-        this.imagemPrevisualizacao = fileReader.result as string;
-        this.referenciaFormulario?.setValue(arquivo);
-        this.referenciaFormulario?.markAsDirty();
-        this.aoAtualizarImagem.emit(this.imagemPrevisualizacao);
-      };
+      this.fazerUploadImagem(arquivo);
+    }
+  }
+
+  public manipularDragOver(evt: any) {
+    evt.preventDefault();
+  }
+
+  public manipularDrop(evt: any) {
+    evt.preventDefault();
+
+    if (evt.dataTransfer.files && evt.dataTransfer.files.length) {
+      const [arquivo] = evt.dataTransfer.files;
+      this.fazerUploadImagem(arquivo);
     }
   }
 }
